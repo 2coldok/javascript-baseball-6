e0a5572611588d0d35b5f151655605f2905b5f6d
coldok <2coldok@gmail.com>
2023-11-26 13:51:46 +0900

    chore : chore

---
+++ src/playground/Playground.js
@@ -0,0 +1,35 @@
+/*export const JAPAN = ['규동', '우동', '미소시루', '스시', '가츠동', '오니기리', '하이라이스', '라멘', '오코노미야끼'];
+export const KOREA = ['김밥', '김치찌개', '쌈밥', '된장찌개', '비빔밥', '칼국수', '불고기', '떡볶이', '제육볶음'];
+export const CHINA = ['깐풍기', '볶음면', '동파육', '짜장면', '짬뽕', '마파두부', '탕수육', '토마토 달걀볶음', '고추잡채'];
+export const ASIA = ['팟타이', '카오 팟', '나시고렝', '파인애플 볶음밥', '쌀국수', '똠얌꿍', '반미', '월남쌈', '분짜'];
+export const WESTERN = ['라자냐', '그라탱', '뇨끼', '끼슈', '프렌치 토스트', '바게트', '스파게티', '피자', '파니니'];*/
+
+export const MENU = {
+  japan : ['규동', '우동', '미소시루', '스시', '가츠동', '오니기리', '하이라이스', '라멘', '오코노미야끼'],
+  korea : ['김밥', '김치찌개', '쌈밥', '된장찌개', '비빔밥', '칼국수', '불고기', '떡볶이', '제육볶음'],
+  china : ['깐풍기', '볶음면', '동파육', '짜장면', '짬뽕', '마파두부', '탕수육', '토마토 달걀볶음', '고추잡채'],
+  asia : ['팟타이', '카오 팟', '나시고렝', '파인애플 볶음밥', '쌀국수', '똠얌꿍', '반미', '월남쌈', '분짜'],
+  western : ['라자냐', '그라탱', '뇨끼', '끼슈', '프렌치 토스트', '바게트', '스파게티', '피자', '파니니'],
+};
+
+const MENU_MAP = new Map()
+  .set(1, MENU.japan)
+  .set(2, MENU.korea)
+  .set(3, MENU.china)
+  .set(4, MENU.asia)
+  .set(5, MENU.western);
+
+const MENU_TYPE = {
+  1 : 'japan',
+  2 : 'korea',
+  3 : 'china',
+  4 : 'asia',
+  5 : 'western',
+  length : 5,
+};
+
+function findType(menu) {
+  const [type, _] = Object.entries(MENU).find(([_, menus]) => menus.includes(menu));
+
+  return type;
+}
